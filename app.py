6e3b5bb5e64adb82b38e5cf913c6aea15c9ee973
Gazerb <gazerb@hotmail.co.uk>
2023-03-04 13:51:21 +0000

    booking html tmplated added

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello Wolrd.... again!"
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/show_bookings")
+def show_bookings():
+    bookings = mongo.db.bookings.find()
+    return render_template("bookings.html", bookings=bookings)
 
 
 if __name__ == "__main__":
