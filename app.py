998acd23ceeaafde9a4939925bacc0daf1bf40dc
Gazerb <gazerb@hotmail.co.uk>
2023-03-05 17:10:49 +0000

    add to the register template

--- app.py
+++ app.py
@@ -30,15 +30,38 @@ def hello():
 @app.route("/show_bookings")
 def show_bookings():
     bookings = list(mongo.db.bookings.find())
-    print(bookings)
     return render_template("bookings.html", bookings=bookings)
 
 
 @app.route("/show_mentors")
 def show_mentors():
     mentors = list(mongo.db.mentors.find())
-    print(mentors)
     return render_template("mentors.html", mentors=mentors)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
