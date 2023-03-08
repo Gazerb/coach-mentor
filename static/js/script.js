3a77ba4ca6f2e4da7215136324c26090bc3ec774
Gazerb <gazerb@hotmail.co.uk>
2023-03-08 11:11:35 +0000

    make a booking page form completed and tidied

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
-    $('.datepicker').datepicker();
+    $('select').formSelect();
+    $(".datepicker").datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+          done: "Select"
+      }
+
+    })
+    
   });
