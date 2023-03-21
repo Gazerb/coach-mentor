0ce11ab4280448a1b18e6f7514f0b2bd61e3329d
Gazerb <gazerb@hotmail.co.uk>
2023-03-21 16:03:07 +0000

    tidy up of code with comments added

--- app.py
+++ app.py
@@ -21,10 +21,14 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+# loads home page on start up
+
 
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+# Route for mentors page
 
 
 @app.route("/show_mentors")
@@ -32,18 +36,20 @@ def show_mentors():
     mentors = list(mongo.db.mentors.find())
     return render_template("mentors.html", mentors=mentors)
 
+# route for register page with code to take details and add them to the DB
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # check if username already exists in db
+        # checks if username already exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
-
+        # collects user details and submits it to the DB
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password")),
@@ -61,6 +67,8 @@ def register():
         return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
+
+# Route for login page
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -80,7 +88,7 @@ def login():
                 return redirect(url_for(
                     "profile", username=session["user"]))
             else:
-                # invalid password match
+                # Alerts user of invalid password
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
@@ -97,11 +105,13 @@ def profile():
     # grab the session user's username from db
     current_user = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    
+
     if session["user"]:
         return render_template("profile.html", username=current_user)
 
     return redirect(url_for("login"))
+
+# Route for user to log out
 
 
 @app.route("/logout")
@@ -110,6 +120,8 @@ def logout():
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
+
+# Route to make a booking with the mentors
 
 
 @app.route("/make_a_booking", methods=["GET", "POST"])
@@ -126,17 +138,22 @@ def make_a_booking():
             "booking_reason": request.form.get("booking_reason"),
             "created_by": session["user"]
         }
+        # Inserts the booking information into the DB
         mongo.db.bookings.insert_one(booking)
         flash("Booking completed, A mentor will be in touch")
         return redirect(url_for("make_a_booking"))
 
     return render_template("make_a_booking.html")
 
+# Route for user to see their bookings
+
 
 @app.route("/show_bookings")
 def show_bookings():
     bookings = list(mongo.db.bookings.find({"created_by": session["user"]}))
     return render_template("bookings.html", bookings=bookings)
+
+# Route for user to edit their booking
 
 
 @app.route("/edit_booking/<bookings_id>", methods=['GET', 'POST'])
@@ -153,12 +170,15 @@ def edit_booking(bookings_id):
             "booking_reason": request.form.get("booking_reason"),
             "created_by": session["user"]
         }
+        # Replaces data of booking to match changes made by the user
         mongo.db.bookings.replace_one({"_id": ObjectId(bookings_id)}, submit)
         flash("Booking Updated")
         return redirect(url_for("show_bookings"))
 
     bookings = mongo.db.bookings.find_one({"_id": ObjectId(bookings_id)})
     return render_template("edit_booking.html", bookings=bookings)
+
+# Route for user to cancel a booking made
 
 
 @app.route("/delete_booking/<bookings_id>")
